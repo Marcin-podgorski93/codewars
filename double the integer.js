0c7de464a284790306f6e0cccdb36b0a4d7f5a8f
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-02-17 17:37:53 +0100

    Double the integer

---
+++ double the integer.js
@@ -0,0 +1,21 @@
+// Code as fast as you can! You need to double the integer and return it.
+// function doubleInteger(i) {
+  
+//     return i;
+//   }
+
+
+//My solution
+
+function doubleInteger(i) {
+    return i * 2;
+}
+
+console.log(doubleInteger(5))
+
+function doubleInteger1(i) {
+    return i.map(num => num * 2)
+
+}
+
+console.log(doubleInteger1([6]))
