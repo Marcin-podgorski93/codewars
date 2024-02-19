802ee83eb8b4e885e4803ec61033fd97094d2bca
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-02-19 17:08:19 +0100

    exces and ohs

--- Powers of 2.js
+++ Powers of 2.js
@@ -25,3 +25,15 @@ function powerOfTwo(n) {
 }
 
 console.log(powerOfTwo(2))
+
+
+
+function powerOfTwo1(n) {
+    let result = [];
+    for (let i = 0; i <= n; i++) {
+        result.push(Math.pow(2, i));
+    }
+    return result;
+}
+
+console.log(powerOfTwo1(2));
