9902ba4e8d14eb9a987be465b3516c95f5e9d988
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-03-04 15:18:23 +0100

    exercise number in array

---
+++ Number in array.js
@@ -0,0 +1,32 @@
+const arr = [[3,21,37], [61,79,101,120], [133,149]];
+
+const inNumberInArrayOfArrays = (number, array) => {
+    for (let i = 0; i < array.length; i++) {
+        const arrays = array[i];
+        console.log(arrays[i])
+        if (arrays.indexOf(number) !== -1) { //lub napisac number) >= 0;
+            console.log(`Liczba ${number} jest w array ${[i]}.`);
+            return true;
+            
+        }
+    }
+    return false;
+}; 
+
+console.log(inNumberInArrayOfArrays(3, arr));
+
+
+// or
+
+
+const inNumberInArrayOfArrays1 = (number, array) => {
+    let found = false;
+    array.forEach(element => {
+        if (element.indexOf(number) !== -1) { //lub napisac number) >= 0;
+            found = true;
+    }
+    });
+    return false;
+}; 
+
+console.log(inNumberInArrayOfArrays(0, arr));
