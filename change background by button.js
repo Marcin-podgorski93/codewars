f0ce2841fc7ad09f7e0e3464ff76e6f257a1ad6c
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-03-05 20:35:56 +0100

    adding change background by button

---
+++ change background by button.js
@@ -0,0 +1,14 @@
+// HTML 
+
+// <button id="switch">Click me!</button>
+
+let color = 'white';
+
+const button = document.getElementById("switch");
+
+
+button.addEventListener("click", () => {
+console.log("Click!!!");
+color = color === 'white' ? 'black' : 'white';
+document.body.style.background = color;
+});
