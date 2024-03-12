6f1dadd4d70d159aa3ca40e81a3ee22f1ba5258e
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-03-12 10:00:19 +0100

    exercise friend or Foe

---
+++ Friend or Foe.js
@@ -0,0 +1,18 @@
+function friend(names){
+    return names.filter(name => name.length === 4);
+  }
+
+  const inputNames = ["Ryan", "Kieran", "Mark"];
+
+  const  outputNames = friend(inputNames);
+
+  console.log(outputNames);
+
+
+//   console.log(friend)
+
+
+//   const friend = ["Ryan", "Kieran", "Mark"];
+
+//   const filterFriend = friend.filter( word => word.length === 4);
+//   console.log(filterFriend)
