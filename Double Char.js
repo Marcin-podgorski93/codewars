3a2e1b6d2c17bfb51c439875c713fc738280d631
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-03-19 23:37:26 +0100

    update

--- Double Char.js
+++ Double Char.js
@@ -15,9 +15,17 @@ function doubleChar(str) {
  console.log(doubleChar("String"))
  console.log(doubleChar("Hello World"))
 
- function doubleChar2(str) {
-    return doubleWord = str.split('').map(word => word.repeat(2)).join('');
- }
+//  function doubleChar2(str) {
+//     return doubleWord = str.split('').map(word => word.repeat(2)).join('');
+//  }
 
- console.log(doubleChar2("String"))
- console.log(doubleChar2("Hello World"))
+//  console.log(doubleChar2("String"))
+//  console.log(doubleChar2("Hello World"))
+
+
+ const char = "String";
+
+ let doubleWord = char.split('').map(word => word.repeat(2)).join(''); 
+//  mozna uzyc zamiast word => word.repeat(2)// word => word + word;
+
+ console.log(doubleWord)
