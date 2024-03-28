afd47c382382aca2f7f33876d5691cc1cf8168a3
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-03-28 23:27:43 +0100

    exercise reverse words

--- find Num in array.js
+++ find Num in array.js
@@ -10,3 +10,35 @@ const isNumberInArrayofArrays = (number, array) => {
 
 console.log(isNumberInArrayofArrays(0, arr));
 console.log(isNumberInArrayofArrays(21, arr));
+
+function isNumberInArrayofArrays1(number, array) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        console.log(element)
+        if (element.includes(number)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+console.log(isNumberInArrayofArrays1(21, arr))
+console.log(isNumberInArrayofArrays1(0, arr))
+
+
+function isNumberInArrayofArrays2(number, array) {
+    let found = false;
+    array.forEach(element => {
+        if (element.includes(number)) {
+            found = true;
+            return;
+        } 
+    });
+    return found;;
+}   
+
+
+console.log(isNumberInArrayofArrays2(21, arr))
+console.log(isNumberInArrayofArrays2(0, arr))
+
